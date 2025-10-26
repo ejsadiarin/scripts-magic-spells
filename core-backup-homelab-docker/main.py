bfcd178cc1a0258040450de38eee1687628db82b
ejsadiarin <ejs@ejsadiarin.com>
2025-10-26 17:36:38 +0800

    feat: local backups to hdd and init script python for backups

---
+++ core-backup-homelab-docker/main.py
@@ -0,0 +1,14 @@
+"""
+This script should:
+    - Raw local backup to /mnt/Backups/core/raw (HDD) via rsync
+    - Raw remote backup to backblaze b2 via restic
+    - Scheduled to run every week or Run manually
+"""
+
+
+def main():
+    print("tite")
+
+
+if __name__ == "__main__":
+    main()
